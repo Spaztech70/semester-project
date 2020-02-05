b4155179572eca27514003de9d962730c50b29e9
Bill Hanson <billh@nmsu.edu>
2020-02-05 13:56:16 -0700

    Create jb-project.py

---
+++ proj/stage3/jb-project.py
@@ -0,0 +1,27 @@
+"""
+
+jb-project.py
+CS 487, Spring 2020
+Semester Project
+5 Feb 2020
+Prof. Cao
+@author: Bill Hanson
+@author: Jay Johnston
+version 0.1
+
+This program is a starter shell   
+
+"""
+#%%
+# Import needed packages
+
+import os
+import sys
+import pandas as pd
+import numpy as np
+import sklearn as sk
+import matplotlib.pyplot as plt
+import seaborn as sns
+from time import perf_counter
+
+#%%
